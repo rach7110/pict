a9ccb6f5551e98f56aa4dc9e9e7b6edb6eb20fe5
Rachel <rachel.loziuk@gmail.com>
2018-10-22 17:17:05 -0500

    fix class name

--- app/Providers/ImageRecognitionServiceProvider.php
+++ app/Providers/ImageRecognitionServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class ImageRecognition extends ServiceProvider
+class ImageRecognitionServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap services.
