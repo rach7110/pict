6975c29f6e0ce567637d4d5056f0e89c9019af61
Rachel <rachel.loziuk@gmail.com>
2018-10-22 17:12:21 -0500

    add service provider for ImageRecognition

--- config/app.php
+++ config/app.php
@@ -171,6 +171,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\ImageRecognitionServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
     ],
@@ -221,6 +222,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        
 
     ],
 
