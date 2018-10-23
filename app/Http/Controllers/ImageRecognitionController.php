43f89289123fcb2910cdca710cae8c469416b0e6
Rachel <rachel.loziuk@gmail.com>
2018-10-23 15:27:07 -0500

    display flash message after image is stored.

--- app/Http/Controllers/ImageRecognitionController.php
+++ app/Http/Controllers/ImageRecognitionController.php
@@ -8,27 +8,50 @@ use App\ImageRecognitionInterface;
 class ImageRecognitionController extends Controller
 {
     protected $imageRecognition;
-     public function __construct(ImageRecognitionInterface $imageRecognitionService)
+    
+    public function __construct(ImageRecognitionInterface $imageRecognitionService)
     {
         $this->imageRecognition = $imageRecognitionService;
-    }   
-     public function create()
-    {
-        return view('images.concepts.create');
     }
-     public function store()
-    {
-        $file = request()->file('image')->store('images');
-        // var_dump(config('filesystems.disks.local.root') . "/" . $file);
-        
-        //TODO: Validate the file contents.
     
-        $response = $this->imageRecognition->send_request($input);
+    public function create()
+    {
+        return view('images.create');
+    }
+    
+    /**
+     * Store the image and send request to ImageReconition service.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //TODO: Validate the file contents.
+
+        if (request()->file('image')->store('images')) {
+            $request->session()->flash('message', 'File saved successfully');
+            $request->session()->flash('alert-class', 'alert-success');
+        } else {
+            $request->session()->flash('message', 'Problem uploading your file. Please try again in a few minutes.');
+            $request->session()->flash('alert-class', 'alert-danger');
+        }
+
+        return redirect('image');
+            
+
+        // var_dump(config('filesystems.disks.local.root'));
+
+
+        return back();
+        
+        // Image analysis.
+        $response = $this->imageRecognition->send_request($file);
          if($response->isSuccessful()) {
             $this->imageRecognition->display_output($response);
         } else {
-            Session::flash('message', 'Response not successful. Error Code: ' . $response->status()->statusCode());
-            Session::flash('alert-class', 'alert-danger');
+            $request->session()->flash('message', 'Response not successful. Error Code: ' . $response->status()->statusCode());
+            $request->session()->flash('class', 'alert-danger');
             return back();
         }
     }
