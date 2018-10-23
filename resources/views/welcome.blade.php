5b320412d8e4471f3df98cc818268d8065c81461
Rachel <rachel.loziuk@gmail.com>
2018-10-22 19:47:32 -0500

    remove default links on welcome page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,12 +91,12 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="{{route('image')}}">Let's begin</a>
+                    {{-- <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a> --}}
                 </div>
             </div>
         </div>
