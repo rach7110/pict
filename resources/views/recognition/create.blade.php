863e4790ff015a26707106e9730f7f3bbc0fa607
Rachel <rachel.loziuk@gmail.com>
2018-10-24 14:09:30 -0500

    add second form field to view

--- resources/views/recognition/create.blade.php
+++ resources/views/recognition/create.blade.php
@@ -6,12 +6,25 @@
     <h1>Image Concepts</h1>
     <h3>This is where we will allow the user to upload image(-s)</h3>
     
-    <form method="POST" action="{{route('image')}}" enctype="multipart/form-data">
+    <form method="POST" action="{{route('recognition')}}" enctype="multipart/form-data">
         {{ csrf_field() }}
+        
+        {{--  Content to train the image recogition model. --}}
         <div class="form-group" >
-            <label for="image">Enter your filename.</label>
+            <label for="content">Content</label>
+            <p><strong>Description:</strong> this is the file you use to analyze all other files. </p>
+            <input type="file" name="content" id="content" >
+
+
+        </div>
+        {{-- Image to be analyzed by model. --}}
+        <div class="form-group" >
+            <label for="image">File</label>
+            <p><strong>Description:</strong> this is the file that is being analyzed for content. </p>
+
             <input type="file" name="image" id="image" >
         </div>
+        {{-- Submit --}}
          <input type="submit" value="Submit" style="margin-top: 30px;">
     </form>
 @stop
