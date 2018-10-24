b098762589aff7aad22a680375448d9e0c38e74b
Rachel <rachel.loziuk@gmail.com>
2018-10-24 13:51:01 -0500

    WIP:ImageRecController

--- app/Http/Controllers/ImageRecognitionController.php
+++ app/Http/Controllers/ImageRecognitionController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\ImageRecognitionInterface;
+use Illuminate\Support\Facades\Storage;
 
 class ImageRecognitionController extends Controller
 {
-    protected $imageRecognition;
+    protected $imageRecognitionSvc;
     
     public function __construct(ImageRecognitionInterface $imageRecognitionService)
     {
-        $this->imageRecognition = $imageRecognitionService;
+        $this->imageRecognitionSvc = $imageRecognitionService;
     }
     
     public function create()
@@ -27,37 +28,48 @@ class ImageRecognitionController extends Controller
      */
     public function store(Request $request)
     {
+        $user_file = request()->file('image');
+        // dd($file->path());
+        // dd($file->extension());
         // Validate the file contents.
         $rules = [
             'image' => 'required|image|max:2000|mimes:,png,jpeg,jpg,gif,svg'
         ];
         
-        $request->validate($rules); 
+        $request->validate($rules);
 
-        if (request()->file('image')->store('images')) {
+        $file = $user_file->store('images');
+
+        // Store the file contents.
+        if ($file) {
+            //TODO: store relative path to DB.
+            $upload = new Upload;
+            $upload->directory = $file;
+
             $request->session()->flash('message', 'File saved successfully');
             $request->session()->flash('alert-class', 'alert-success');
+            dd(Storage::get($file)->path());
+
         } else {
             $request->session()->flash('message', 'Problem uploading your file. Please try again in a few minutes.');
             $request->session()->flash('alert-class', 'alert-danger');
         }
 
-        return redirect('image');
-            
 
+
+        // $this->analyzeImage($file);
+        // return redirect('image');
         // var_dump(config('filesystems.disks.local.root'));
+    }
 
+    public function analyzeImage($image)
+    {
+        $response = $this->imageRecognitionSvc->send_request($image);
 
-        return back();
-        
-        // Image analysis.
-        $response = $this->imageRecognition->send_request($file);
          if($response->isSuccessful()) {
-            $this->imageRecognition->display_output($response);
+            return $this->imageRecognitionSvc->display_output($response);
         } else {
-            $request->session()->flash('message', 'Response not successful. Error Code: ' . $response->status()->statusCode());
-            $request->session()->flash('class', 'alert-danger');
-            return back();
+            return 'Response not successful. Error Code: ' . $response->status()->statusCode();
         }
     }
 } 
